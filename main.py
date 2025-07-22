a6531de14c059c51e1f9224442ad13ed1cc208e5
Triopticon <kenneth@ks81.com>
2025-07-23 00:42:00 +0200

    CH2: Data Analysis - L5: Refactor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from stats import get_num_words
+
 def main():
     file_contents = get_book_text("books/frankenstein.txt")
-    num_words = count_words(file_contents)
+    num_words = get_num_words(file_contents)
     print(f"{num_words} words found in the document")
 
     
@@ -9,11 +11,5 @@ def get_book_text(path_to_file):
         file_contents = f.read()
 
     return file_contents
-
-def count_words(text_to_count):
-    words = text_to_count.split()
-    word_count = len(words)
-
-    return word_count
 
 main()
---
+++ stats.py
@@ -0,0 +1,5 @@
+def get_num_words(text_to_count):
+    words = text_to_count.split()
+    word_count = len(words)
+
+    return word_count
