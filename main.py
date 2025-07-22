dbeb4938045fb7afd90df6fd82f76f71f30e7201
Triopticon <kenneth@ks81.com>
2025-07-23 00:34:32 +0200

    Count Words

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+def main():
+    file_contents = get_book_text("books/frankenstein.txt")
+    num_words = count_words(file_contents)
+    print(f"{num_words} words found in the document")
+
+    
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
-        print(f.read())
+        file_contents = f.read()
 
-def main():
-    get_book_text("books/frankenstein.txt")
+    return file_contents
+
+def count_words(text_to_count):
+    words = text_to_count.split()
+    word_count = len(words)
+
+    return word_count
 
 main()
